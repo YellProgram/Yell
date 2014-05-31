58135d8e6cb4d5ce02c52b00288862cd2ea4d855
Arkadiy Simonov <aglietto@gmail.com>
2014-05-31 14:59:28 +0200

    Added license to each file header

--- src/FormulaParser.h
+++ src/FormulaParser.h
@@ -1,10 +1,20 @@
 /*
- *  FormulaParser.h
- *  diffuser_y
- *
- *  Created by Arkadiy Simonov on 3/15/11.
- *  Copyright 2011 ETH Zurich. All rights reserved.
- *
+ Copyright Arkadiy Simonov, Thomas Weber, ETH Zurich 2014
+ 
+ This file is part of Yell.
+ 
+ Yell is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ 
+ Yell is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with Yell.  If not, see <http://www.gnu.org/licenses/>.
  */
 #ifndef H_FORMULA_PARSER
 #define H_FORMULA_PARSER
